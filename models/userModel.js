5df44ac2c2a2aefce494ff7145dae488a39bbdaa
David Carrillo <carrillod421@gmail.com>
2025-01-21 11:24:36 -0600

    contrasena encriptada

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,14 @@
 const db = require('../config/db');
-
+const bcrypt = require('bcryptjs');
 const User = {
 
     getall: ( callback) => {
+       
         db.query('SELECT * FROM users', callback);
     },
     create: (usuario, correo, contrasena, callback) => {
-        db.query('INSERT INTO users (usuario, correo, contrasena) VALUES (?, ?, ?)', [usuario, correo, contrasena], callback);
+        const hashedPassword = bcrypt.hashSync(contrasena, 10);
+        db.query('INSERT INTO users (usuario, correo, contrasena) VALUES (?, ?, ?)', [usuario, correo, hashedPassword], callback);
     },
 
     findByEmail: (correo, callback) => {
