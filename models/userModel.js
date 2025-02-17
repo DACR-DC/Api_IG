6550bb17f14459601a897eb77d5b15d3fbf6c631
David Carrillo <carrillod421@gmail.com>
2025-02-17 11:03:17 -0600

    actualizacion login logout

--- models/userModel.js
+++ models/userModel.js
@@ -19,13 +19,37 @@ const User = {
         db.query('SELECT * FROM users WHERE id = ?', [id], callback);
     },
 
-    getPrivileges: (id, callback) => {
-        db.query('SELECT nombre FROM privilegios WHERE id = ?', [id], callback);
-    },
+    // getPrivileges: (id, callback) => {
+    //     db.query('SELECT nombre FROM privilegios WHERE id = ?', [id], callback);
+    // },
 
-    delete:(id) => {
-        db.query('DELETE FROM users WHERE id = ?', [id]);
-     }
+    // delete:(id) => {
+    //     db.query('DELETE FROM users WHERE id = ?', [id]);
+    //  },
+
+    //  saveSessionToken: (userId, token, expiresAt, callback) => {
+    //     db.query(
+    //         `INSERT INTO token_sesion (id_user, token, expires_at) 
+    //          VALUES (?, ?, ?) 
+    //          ON DUPLICATE KEY UPDATE token = ?, expires_at = ?`,
+    //         [userId, token, expiresAt, token, expiresAt],
+    //         callback
+    //     );
+    // },
+
+    // getSessionToken: (userId, callback) => {
+    //     db.query(
+    //         `SELECT token FROM token_sesion 
+    //          WHERE id_user = ? AND expires_at > NOW()`,
+    //         [userId],
+    //         callback
+    //     );
+    // },
+
+    // deleteSessionTokens: (userId, callback) => {
+    //     db.query('DELETE FROM token_sesion WHERE id_user = ?', [userId], callback);
+    // }
+
 };
 
 module.exports = User;
